a7c79e5c024a1289d83d835946e464f3276d0130
Gino <gino@ginos-air.home>
2018-06-09 13:52:39 +0100

    fix formatting

--- rugby_score.py
+++ rugby_score.py
@@ -1,40 +1,40 @@
 
 #! python
 
-# work out: 5t + 7c + 3p/g = score
+
 
 ###################################################################
 import sys
 
 
 def findScore(score):
+	# work out: 5t + 7c + 3p/g = score
 
 	# setup
 	max_tries = int(score)//5
 	max_convs = int(score)//7
 	max_penalties = int(score)//3
-
 	combs =[]
-
+	# cycle each and find
 	for i in range(max_penalties+1):
 		if i == 0:
 			t = 0
 			c = 0
 			if 3*int(i) + 5*int(t) + 7*int(c) == int(score) and score > 0:
-				found_comb = 'Tries+Conv: %d - Tries: %d - Penalties/Drop Goals): %d' % (c, i, t)
+				found_comb = 'Tries+Conv: %d - Tries: %d - Penalties/Drop Goals): %d' % (c, t, i)
 				combs.append(found_comb)
 
 		for t in range(max_tries+1):
 			if i == 0 and t == 0:
 				c = 0
 				if 3*int(i) + 5*int(t) + 7*int(c) == int(score) and score > 0:
-					found_comb = 'Tries+Conv: %d - Tries: %d - Penalties/Drop Goals): %d' % (c, i, t)
+					found_comb = 'Tries+Conv: %d - Tries: %d - Penalties/Drop Goals): %d' % (c, t, i)
 					combs.append(found_comb)
 
 			for c in range (max_convs+1):
 				# print 'converted tries', s      # Only needed for spotting errors
 				if 3*int(i) + 5*int(t) + 7*int(c) == int(score) and score > 0:
-					found_comb = 'Tries+Conv: %d - Tries: %d - Penalties/Drop Goals): %d' % (c, i, t)
+					found_comb = 'Tries+Conv: %d - Tries: %d - Penalties/Drop Goals): %d' % (c, t, i)
 					combs.append(found_comb)
 
 	return combs
